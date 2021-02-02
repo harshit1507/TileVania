ba1b7e65a24baeb8e0f1ba33c22d23d96850586f
harshit1507 <harshit1507@yahoo.com>
2021-02-02 16:12:40 +0530

    Enemy Added

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+
+    Rigidbody2D myRigidbody;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+    }
+
+    private void Move()
+    {
+        if (IsFacingRight())
+        {
+            myRigidbody.velocity = new Vector2(moveSpeed, 0);
+        }
+        else
+        {
+            myRigidbody.velocity = new Vector2(-moveSpeed, 0);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(myRigidbody.velocity.x)), 1f);
+    }
+
+    bool IsFacingRight()
+    {
+        return transform.localScale.x > 0;
+    }
+}
