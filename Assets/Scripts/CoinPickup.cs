594c3ab6334c9d36d2a8f296bbb21e70629cc066
harshit1507 <harshit1507@yahoo.com>
2021-02-03 23:54:11 +0530

    Sound for Coins Pickup

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    [SerializeField] AudioClip coinPickUpSFX;
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        AudioSource.PlayClipAtPoint(coinPickUpSFX, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
